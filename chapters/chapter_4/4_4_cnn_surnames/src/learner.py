c288bebccfa7f79e0d5b13245353da194ccb630f
rristo <ristohinno@gmail.com>
2020-01-06 19:47:22 +0200

    made func for train eval

--- chapters/chapter_4/4_4_cnn_surnames/src/learner.py
+++ chapters/chapter_4/4_4_cnn_surnames/src/learner.py
@@ -21,7 +21,7 @@ class Learner(object):
         self.classifier = classifier
 
         self.loss_func = nn.CrossEntropyLoss(weight=self.dataset.class_weights)
-        #self.optimizer = optim.Adam(classifier.parameters(), lr=args.learning_rate)
+        # self.optimizer = optim.Adam(classifier.parameters(), lr=args.learning_rate)
         self.optimizer = RAdam(classifier.parameters(), lr=args.learning_rate)
         self.scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer=self.optimizer,
                                                               mode='min', factor=0.5,
@@ -46,11 +46,51 @@ class Learner(object):
         return epoch_bar, train_bar, val_bar
 
     def _add_update_args(self, **kwargs):
-        #turn into dict
+        # turn into dict
         args_dict = vars(self.args)
-        #changes also values in self.args
+        # changes also values in self.args
         for key, value in kwargs.items():
             args_dict[key] = value
+
+    def train_eval_epoch(self, batch_generator, epoch_index, progress_bar, train_val='train'):
+        if train_val not in ['train', 'val']:
+            raise ValueError
+
+        running_loss = 0.0
+        running_acc = 0.0
+        for batch_index, batch_dict in enumerate(batch_generator):
+            # the training routine is these 5 steps:
+
+            # --------------------------------------
+            # step 1. zero the gradients
+            self.optimizer.zero_grad()
+
+            # step 2. compute the output
+            y_pred = self.classifier(batch_dict['x_surname'])
+
+            # step 3. compute the loss
+            loss = self.loss_func(y_pred, batch_dict['y_nationality'])
+            loss_t = loss.item()
+            running_loss += (loss_t - running_loss) / (batch_index + 1)
+
+            if train_val == 'train':
+                # step 4. use loss to produce gradients
+                loss.backward()
+
+                # step 5. use optimizer to take gradient step
+                self.optimizer.step()
+            # -----------------------------------------
+            # compute the accuracy
+            acc_t = compute_accuracy(y_pred, batch_dict['y_nationality'])
+            running_acc += (acc_t - running_acc) / (batch_index + 1)
+
+            # update bar
+            progress_bar.set_postfix(loss=running_loss, acc=running_acc,
+                                     epoch=epoch_index)
+            progress_bar.update()
+
+        self.train_state[f'{train_val}_loss'].append(running_loss)
+        self.train_state[f'{train_val}_acc'].append(running_acc)
 
     def train(self, **kwargs):
         self._add_update_args(**kwargs)
@@ -68,73 +108,17 @@ class Learner(object):
                 batch_generator = generate_batches(self.dataset,
                                                    batch_size=self.args.batch_size,
                                                    device=self.args.device)
-                running_loss = 0.0
-                running_acc = 0.0
                 self.classifier.train()
-
-                for batch_index, batch_dict in enumerate(batch_generator):
-                    # the training routine is these 5 steps:
-
-                    # --------------------------------------
-                    # step 1. zero the gradients
-                    self.optimizer.zero_grad()
-
-                    # step 2. compute the output
-                    y_pred = self.classifier(batch_dict['x_surname'])
-
-                    # step 3. compute the loss
-                    loss = self.loss_func(y_pred, batch_dict['y_nationality'])
-                    loss_t = loss.item()
-                    running_loss += (loss_t - running_loss) / (batch_index + 1)
-
-                    # step 4. use loss to produce gradients
-                    loss.backward()
-
-                    # step 5. use optimizer to take gradient step
-                    self.optimizer.step()
-                    # -----------------------------------------
-                    # compute the accuracy
-                    acc_t = compute_accuracy(y_pred, batch_dict['y_nationality'])
-                    running_acc += (acc_t - running_acc) / (batch_index + 1)
-
-                    # update bar
-                    train_bar.set_postfix(loss=running_loss, acc=running_acc,
-                                          epoch=epoch_index)
-                    train_bar.update()
-
-                self.train_state['train_loss'].append(running_loss)
-                self.train_state['train_acc'].append(running_acc)
-
+                self.train_eval_epoch(batch_generator, epoch_index, train_bar)
                 # Iterate over val dataset
-
                 # setup: batch generator, set loss and acc to 0; set eval mode on
                 self.dataset.set_split('val')
                 batch_generator = generate_batches(self.dataset,
                                                    batch_size=self.args.batch_size,
                                                    device=self.args.device)
-                running_loss = 0.
-                running_acc = 0.
+
                 self.classifier.eval()
-
-                for batch_index, batch_dict in enumerate(batch_generator):
-                    # compute the output
-                    y_pred = self.classifier(batch_dict['x_surname'])
-
-                    # step 3. compute the loss
-                    loss = self.loss_func(y_pred, batch_dict['y_nationality'])
-                    loss_t = loss.item()
-                    running_loss += (loss_t - running_loss) / (batch_index + 1)
-
-                    # compute the accuracy
-                    acc_t = compute_accuracy(y_pred, batch_dict['y_nationality'])
-                    running_acc += (acc_t - running_acc) / (batch_index + 1)
-                    val_bar.set_postfix(loss=running_loss, acc=running_acc,
-                                        epoch=epoch_index)
-                    val_bar.update()
-
-                self.train_state['val_loss'].append(running_loss)
-                self.train_state['val_acc'].append(running_acc)
-
+                self.train_eval_epoch(batch_generator, epoch_index, val_bar, 'val')
                 train_state = update_train_state(args=self.args, model=self.classifier,
                                                  train_state=self.train_state)
 
