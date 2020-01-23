11e9a6aca458301cf3200a5b79d317a0b8a4f88a
rristo <ristohinno@gmail.com>
2020-01-23 20:35:12 +0200

    conditioned first pieces

--- chapters/chapter_7/7_3_surname_generation/src/learner.py
+++ chapters/chapter_7/7_3_surname_generation/src/learner.py
@@ -3,7 +3,6 @@ from pathlib import Path
 
 import torch
 from torch import optim
-import torch.nn as nn
 from tqdm import tqdm_notebook as tqdm
 from functools import partial
 
@@ -288,3 +287,48 @@ class Learner(object):
             learner.scheduler = learner_states['scheduler']
             learner.train_state = learner_states['train_state']
         return learner
+
+
+class ConstrainedLearner(Learner):
+    def __init__(self, args, dataset, vectorizer, model):
+        super().__init__(self, args, dataset, vectorizer, model)
+
+    def train_eval_epoch(self, batch_generator, epoch_index, progress_bar, train_val='train'):
+        if train_val not in ['train', 'val']:
+            raise ValueError
+
+        running_loss = 0.0
+        running_acc = 0.0
+        for batch_index, batch_dict in enumerate(batch_generator):
+            # the training routine is these 5 steps:
+
+            # --------------------------------------
+            # step 1. zero the gradients
+            self.optimizer.zero_grad()
+
+            # step 2. compute the output
+            y_pred = self.model(x_in=batch_dict['x_data'], nationality_index=batch_dict['class_index'])
+
+            # step 3. compute the loss
+            loss = self.loss_func(y_pred, batch_dict['y_target'], self.mask_index)
+            loss_t = loss.item()
+            running_loss += (loss_t - running_loss) / (batch_index + 1)
+
+            if train_val == 'train':
+                # step 4. use loss to produce gradients
+                loss.backward()
+
+                # step 5. use optimizer to take gradient step
+                self.optimizer.step()
+            # -----------------------------------------
+            # compute the accuracy
+            acc_t = compute_accuracy(y_pred, batch_dict['y_target'], self.mask_index)
+            running_acc += (acc_t - running_acc) / (batch_index + 1)
+
+            # update bar
+            progress_bar.set_postfix(loss=running_loss, acc=running_acc,
+                                     epoch=epoch_index)
+            progress_bar.update()
+
+        self.train_state[f'{train_val}_loss'].append(running_loss)
+        self.train_state[f'{train_val}_acc'].append(running_acc)
--- chapters/chapter_7/7_3_surname_generation/src/model.py
+++ chapters/chapter_7/7_3_surname_generation/src/model.py
@@ -119,3 +119,108 @@ def decode_samples(sampled_indices, vectorizer):
                 surname += vocab.lookup_index(sample_item)
         decoded_surnames.append(surname)
     return decoded_surnames
+
+
+class SurnameGenerationModelConditioned(nn.Module):
+    def __init__(self, char_embedding_size, char_vocab_size, num_nationalities,
+                 rnn_hidden_size, batch_first=True, padding_idx=0, dropout_p=0.5):
+        """
+        Args:
+            char_embedding_size (int): The size of the character embeddings
+            char_vocab_size (int): The number of characters to embed
+            num_nationalities (int): The size of the prediction vector
+            rnn_hidden_size (int): The size of the RNN's hidden state
+            batch_first (bool): Informs whether the input tensors will
+                have batch or the sequence on the 0th dimension
+            padding_idx (int): The index for the tensor padding;
+                see torch.nn.Embedding
+            dropout_p (float): the probability of zeroing activations using
+                the dropout method.  higher means more likely to zero.
+        """
+        super(SurnameGenerationModelConditioned, self).__init__()
+
+        self.char_emb = nn.Embedding(num_embeddings=char_vocab_size,
+                                     embedding_dim=char_embedding_size,
+                                     padding_idx=padding_idx)
+
+        self.nation_emb = nn.Embedding(num_embeddings=num_nationalities,
+                                       embedding_dim=rnn_hidden_size)
+
+        self.rnn = nn.GRU(input_size=char_embedding_size,
+                          hidden_size=rnn_hidden_size,
+                          batch_first=batch_first)
+
+        self.fc = nn.Linear(in_features=rnn_hidden_size,
+                            out_features=char_vocab_size)
+
+        self._dropout_p = dropout_p
+
+    def forward(self, x_in, nationality_index, apply_softmax=False):
+        """The forward pass of the model
+
+        Args:
+            x_in (torch.Tensor): an input data tensor.
+                x_in.shape should be (batch, max_seq_size)
+            nationality_index (torch.Tensor): The index of the nationality for each data point
+                Used to initialize the hidden state of the RNN
+            apply_softmax (bool): a flag for the softmax activation
+                should be false if used with the Cross Entropy losses
+        Returns:
+            the resulting tensor. tensor.shape should be (batch, char_vocab_size)
+        """
+        x_embedded = self.char_emb(x_in)
+
+        # hidden_size: (num_layers * num_directions, batch_size, rnn_hidden_size)
+        nationality_embedded = self.nation_emb(nationality_index).unsqueeze(0)
+
+        y_out, _ = self.rnn(x_embedded, nationality_embedded)
+
+        batch_size, seq_size, feat_size = y_out.shape
+        y_out = y_out.contiguous().view(batch_size * seq_size, feat_size)
+
+        y_out = self.fc(F.dropout(y_out, p=self._dropout_p))
+
+        if apply_softmax:
+            y_out = F.softmax(y_out, dim=1)
+
+        new_feat_size = y_out.shape[-1]
+        y_out = y_out.view(batch_size, seq_size, new_feat_size)
+
+        return y_out
+
+
+def sample_from_model_conditioned(model, vectorizer, nationalities, sample_size=20,
+                      temperature=1.0):
+    """Sample a sequence of indices from the model
+
+    Args:
+        model (SurnameGenerationModel): the trained model
+        vectorizer (SurnameVectorizer): the corresponding vectorizer
+        nationalities (list): a list of integers representing nationalities
+        sample_size (int): the max length of the samples
+        temperature (float): accentuates or flattens
+            the distribution.
+            0.0 < temperature < 1.0 will make it peakier.
+            temperature > 1.0 will make it more uniform
+    Returns:
+        indices (torch.Tensor): the matrix of indices;
+        shape = (num_samples, sample_size)
+    """
+    num_samples = len(nationalities)
+    begin_seq_index = [vectorizer.char_vocab.begin_seq_index
+                       for _ in range(num_samples)]
+    begin_seq_index = torch.tensor(begin_seq_index,
+                                   dtype=torch.int64).unsqueeze(dim=1)
+    indices = [begin_seq_index]
+    nationality_indices = torch.tensor(nationalities, dtype=torch.int64).unsqueeze(dim=0)
+    h_t = model.nation_emb(nationality_indices)
+
+    for time_step in range(sample_size):
+        x_t = indices[time_step]
+        x_emb_t = model.char_emb(x_t)
+        rnn_out_t, h_t = model.rnn(x_emb_t, h_t)
+        prediction_vector = model.fc(rnn_out_t.squeeze(dim=1))
+        probability_vector = F.softmax(prediction_vector / temperature, dim=1)
+        indices.append(torch.multinomial(probability_vector, num_samples=1))
+    indices = torch.stack(indices).squeeze().permute(1, 0)
+    return indices
